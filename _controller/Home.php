ecd67a2815cdccf6a78b4bd8e16d051e7dc00248
grawson <grawson2@jhu.edu>
2017-01-28 14:50:36 -0500

    black theme attempt 2

--- _controller/Home.php
+++ _controller/Home.php
@@ -10,8 +10,12 @@ class Home extends Base_Controller {
     ];
 
     private $bio_body = [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porta, lacus non commodo varius, nisl massa suscipit ante, quis malesuada ligula felis in justo.",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porta, lacus non commodo varius, nisl massa suscipit ante, quis malesuada ligula felis in justo."
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porta, lacus non commodo varius, nisl massa suscipit ante, quis malesuada ligula felis in justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porta, lacus non commodo varius, nisl massa suscipit ante, quis malesuada ligula felis in justo.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porta, lacus non commodo varius, nisl massa suscipit ante, quis malesuada ligula felis in justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porta, lacus non commodo varius, nisl massa suscipit ante, quis malesuada ligula felis in justo."
+    ];
+
+    private $colors = [
+        "#ff9279", "#b1ffb7"
     ];
 
     function __construct() {
@@ -21,6 +25,8 @@ class Home extends Base_Controller {
         for ($i=0; $i < count($this->bio_title); $i++) {
             $this->data["bio"][$i]["bio-title"] = $this->bio_title[$i];
             $this->data["bio"][$i]["bio-body"] = $this->bio_body[$i];
+            $this->data["bio"][$i]["color"] = $this->colors[$i];
+
         }
     }
 }
